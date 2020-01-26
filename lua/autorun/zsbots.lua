7e0793e07979ff619caed243ae753b41001ad031
randomperson189 <32617562+randomperson189@users.noreply.github.com>
2020-01-27 01:25:51 +0800

    Add files via upload

--- lua/autorun/zsbots.lua
+++ lua/autorun/zsbots.lua
@@ -1,9 +1,9 @@
 if SERVER then
-	include ("zsbots/server/cvars.lua")
-	include ("zsbots/server/functions.lua")
-	include ("zsbots/server/ai.lua")
+	include ("zsbots/cvars.lua")
+	include ("zsbots/functions.lua")
+	include ("zsbots/ai.lua")
 end
 
 if CLIENT then
-	--include ("zsbots/client/debughud.lua")
+	--include ("zsbots/debughud.lua")
 end
