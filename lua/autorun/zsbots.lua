ffbcc871ef0558b0390ff526013141be2f410290
A Random Person <32617562+randomperson189@users.noreply.github.com>
2020-08-26 18:53:28 +0800

    Add files via upload

---
+++ lua/autorun/zsbots.lua
@@ -0,0 +1,10 @@
+if SERVER then
+	include ("zsbots/cvars.lua")
+	include ("zsbots/functions.lua")
+	include ("zsbots/ai.lua")
+	include ("zsbots/autobotspawn.lua")
+end
+
+if CLIENT then
+	--include ("zsbots/debughud.lua")
+end
