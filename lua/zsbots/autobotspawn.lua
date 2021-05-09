b66d0cd6aae8c2d34be3250865a7362a3c4c0ac0
A Random Person <32617562+randomperson189@users.noreply.github.com>
2021-05-09 17:47:07 +0800

    Changelog:
    - Added a Lua file for task checking for better organisation
    - Human Bots now heal themselves when low on HP
    - Cleaned up hook functions a bit
    - Fixed bot auto spawn sometimes not working properly

--- lua/zsbots/autobotspawn.lua
+++ lua/zsbots/autobotspawn.lua
@@ -1,14 +1,12 @@
-if botsSpawned == nil then botsSpawned = false end
-
-local function ZSPlayerInitialSpawn(ply)
-	if !botsSpawned then
+hook.Add("PlayerInitialSpawn", "ZSPlayerInitialSpawn", function(ply)
+	if botsSpawned == nil then
+		print(ply)
+		botsSpawned = true
+		
 		if GetConVar( "zs_bot_autospawn_count" ):GetInt() > 0 then
 			for i = 1, GetConVar( "zs_bot_autospawn_count" ):GetInt() do
 				player.CreateZSBot()
 			end
 		end
-		
-		botsSpawned = true
 	end
-end
-hook.Add("PlayerInitialSpawn", "ZSPlayerInitialSpawn", ZSPlayerInitialSpawn)
+end )
